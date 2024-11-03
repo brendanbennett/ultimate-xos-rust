4d3299ef94f41eb569be635e9ee505a543005733
Brendan <bennett.brendan@yahoo.co.uk>
2024-11-03 16:26:02 +0000

    Expand

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,9 @@ use rand::seq::SliceRandom;
 use rand::prelude::*;
 use itertools::Itertools;
 use game::{Game, GameStatus};
-use ego_tree::{Tree, NodeId};
+use ego_tree::{NodeId, NodeMut, Tree};
 
-struct Node {
+struct GameNode {
     num_visits: u32,
     prior_prob: f32,
     total_value: f32,
@@ -21,7 +21,7 @@ struct Node {
     num_children: u32
 }
 
-impl Node {
+impl GameNode {
     fn new(prior_prob: f32, game_state: Game, previous_action: Option<Position>) -> Self {
         Self {
             num_visits: 0,
@@ -36,15 +36,11 @@ impl Node {
     fn is_terminal(&self) -> bool {
         !matches!(self.game_state.status(), GameStatus::InProgress { player: _ })
     }
-
-    fn is_expanded(&self) -> bool {
-        self.num_children != 0 || self.is_terminal()
-    }
 }
 
 
 
-type MCTSTree = Tree<Node>;
+type MCTSTree = Tree<GameNode>;
 
 struct MCTS {
     tree: MCTSTree,
@@ -52,21 +48,36 @@ struct MCTS {
 
 impl MCTS {
     fn expand(&mut self, leaf_node_id: NodeId) -> f32 {
-        let leaf_node = self.tree.get(leaf_node_id).unwrap();
+        let mut leaf_node: NodeMut<'_, GameNode> = self.tree.get_mut(leaf_node_id).unwrap();
         if leaf_node.value().is_terminal() {
             let status=  leaf_node.value().game_state.status();
             // Because can only win on your own move
             return status.clone().into()
-        } else if !leaf_node.value().is_expanded() {
+        } else {
+            if leaf_node.value().num_children != 0 {panic!("leaf node already has children! (Probably already expanded)")};
             let valid_moves = leaf_node.value().game_state.valid_moves();
 
-            for valid_move in valid_moves {
-                let mut child_state = self.tree.get(leaf_node_id).unwrap().value().game_state.clone();
-                child_state.take_turn(&valid_move);
+            // Would be calculated from NN
+            let prior_probs: Vec<f32> = vec![0.0; valid_moves.len()];
+            let value: f32 = 0.0;
+
+            leaf_node.value().num_children += valid_moves.len() as u32;
+
+            for (valid_move, prior_prob) in valid_moves.iter().zip(prior_probs.iter()) {
+                let mut child_state = leaf_node.value().game_state.clone();
+                let child_status = child_state.take_turn(&valid_move);
                 // init node, evaluate children an take mean of values as action value
+                let child_node = GameNode::new(*prior_prob, child_state.clone(), Some(valid_move.clone()));
+                
+                leaf_node.append(child_node);
             };
+
+            return value
         }
-        0.0
+    }
+    
+    fn select(&self) -> NodeId {
+        
     }
 }
 
@@ -74,7 +85,7 @@ impl Default for MCTS {
     fn default() -> Self {
         Self {
             tree: MCTSTree::new(
-                Node::new(
+                GameNode::new(
                     0.0, 
                     Game::default(),
                     None,
