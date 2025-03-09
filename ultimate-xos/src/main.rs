878677efd3cb09cb71a2f2a251b976eee14db214
Brendan <bennett.brendan@yahoo.co.uk>
2025-03-09 23:34:32 +0000

    tiny refactor

--- ultimate-xos/src/main.rs
+++ ultimate-xos/src/main.rs
@@ -19,36 +19,40 @@ use tch::Kind;
 
 use policies::{RandomAgent, XONNAgent};
 
+fn generate_new_games() {
+    let device = tch::Device::Cpu;
+    // Generate random games for initial data
+    let rng = rand::thread_rng();
+    let mut agent = RandomAgent { rng };
+
+    let n_games = 1000;
+
+    let replay = self_play(&mut agent, n_games, 800, false);
+    let replay_augmented = replay.augmented();
+
+    println!("Replay augmented from {} to {}", replay.len(), replay_augmented.len());
+    // for i in 0..8 {
+    //     let print_idx = i+64;
+    //     println!("{}\n{}\n{}", replay_augmented.games[print_idx], XOGame::displays(replay_augmented.policies[print_idx].format_to_print()), replay_augmented.values[print_idx]);
+    // }
+
+    let replay_data: ReplayBufferTensorData = replay_augmented.into();
+    replay_data.save_to_file(Path::new("random_games_2.ot")).unwrap();
+}
+
 fn main() {
     let device = tch::Device::Cpu;
-    // // Generate random games for initial data
-    // let rng = rand::thread_rng();
-    // let mut agent = RandomAgent { rng };
-
-    // let n_games = 1000;
-
-    // let replay = self_play(&mut agent, n_games, 800, false);
-    // let replay_augmented = replay.augmented();
-
-    // println!("Replay augmented from {} to {}", replay.len(), replay_augmented.len());
-    // // for i in 0..8 {
-    // //     let print_idx = i+64;
-    // //     println!("{}\n{}\n{}", replay_augmented.games[print_idx], XOGame::displays(replay_augmented.policies[print_idx].format_to_print()), replay_augmented.values[print_idx]);
-    // // }
-
-    // let replay_data: ReplayBufferTensorData = replay_augmented.into();
-    // replay_data.save_to_file(Path::new("random_games_2.ot")).unwrap();
-
-    // // Train NN
-    // let mut replay_data = ReplayBufferTensorData::load_from_file(Path::new("random_games_2.ot"), device).unwrap();
-    // println!("Cuda available: {:?}", tch::Cuda::is_available());
-    // println!("Cudnn available: {}", tch::Cuda::cudnn_is_available());
     
-    // let batch_size = 32;
-    // let epochs = 100;
-    // let vs = nn::VarStore::new(device);
-    // train_on_replay::<XONNAgent, XOGame, 81>(&vs, &replay_data, batch_size, epochs, 0.8);
-    // vs.save("model_0.ot".to_string()).expect("Save Failed");
+    // Train NN
+    let mut replay_data = ReplayBufferTensorData::load_from_file(Path::new("random_games_2.ot"), device).unwrap();
+    println!("Cuda available: {:?}", tch::Cuda::is_available());
+    println!("Cudnn available: {}", tch::Cuda::cudnn_is_available());
+    
+    let batch_size = 32;
+    let epochs = 100;
+    let vs = nn::VarStore::new(device);
+    train_on_replay::<XONNAgent, XOGame, 81>(&vs, &replay_data, batch_size, epochs, 0.8);
+    vs.save("model_0.ot".to_string()).expect("Save Failed");
 
     // evaluation
     let rng = rand::thread_rng();
